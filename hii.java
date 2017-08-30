b96ab9b12d407d04dd868a384b144a6d82bd8d37
Vishal Krishan <vishalkrishan4542@gmail.com>
2017-08-30 22:31:43 +0530

    New Codes

---
+++ hii.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Number	{
+	public static void main(String args[])	{
+		Scanner in= new Scanner(System.in); 
+		int n = in.nextInt();
+		System.out.println(n);
+	}
+}
+
